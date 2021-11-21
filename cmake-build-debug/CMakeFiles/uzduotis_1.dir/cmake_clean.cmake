73ba5563e89c5b1054ee7e15ece9b094dea5181d
Jurate Breimelyte <juratebreimelyte@Jurates-MacBook-Pro-2017.local>
2021-11-21 17:34:06 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/uzduotis_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/uzduotis_1.dir/main.cpp.o"
+  "uzduotis_1"
+  "uzduotis_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/uzduotis_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
